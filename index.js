36f524d67c3bc37dc12ba750c14faa3c7cec1670
Hvierma20 <inghermesvierma@hotmail.com>
2020-07-29 20:59:19 -0500

    Latest version, need help!

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-//------------------------ Game Project---------------------------
+
 //Do you remember the game Battleship we created before? well .... it is time to make it with the DOM!!
 //We are providing you with the design of a board (in the DOM) for a player1, you have to create the board for the player2 using the id property 'board_player2' -> it is the second list (ul) in your index.html file
-
-//Now each time the turn player clicks on any cell of the opponent's board (you have to verify if the player is clicking the right board) 
+//Now each time the turn player clicks on any cell of the opponent's board (you have to verify if the player is clicking the right board)
 //the program needs to verify if there is an opponent's ship in that cell. If it is then the opponent has one less ship
 //We want you to store the data of each player in two Player objects. Each object has to store: name, remaining boats, and their respective board.
 //Each board needs to be initialized randomly with '0' and four '1' wich means the state of the cell. Numbers 1 are representing the 4 positions of the player's ships
@@ -11,118 +10,160 @@
 //in the index.html file you are going to find 4 more ids: 'name_player1' , 'name_player2' , 'ships_player1' , 'ships_player2'. We want to see the information of each player in the respective elements
 //As our previous Battleship, the winner is the player that hits the 4 opponent's ships first
 //one more Thing create a 'reset' and a 'new game' buttons as childs of the element with the id 'buttons'. the reset button has to start the game again and the new game create a new game with new players and a new random board.
-
-  // Step 1: Create Players
-  let player1 = {
-    name: prompt('Player 1 enter your name'), //Ask the players for their names (use propmt)
-    shipCount: parseInt(4),
-    gameBoard: [ 
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-    ]
-    }
-  let player2 = {
-    name: prompt('Player 2 enter your name'), //Ask the players for their names (use propmt)
-    shipCount: parseInt(4),
-    gameBoard: [ 
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-    ]
-    }
-    
-const board_Player1 = document.getElementById('board_player1');
-const board_Player2 = document.getElementById('board_player2');
-
-if (player1.name && player2.name != null ) {
-  document.getElementById('name_player1').innerHTML = player1.name;
-  document.getElementById('name_player2').innerHTML = player2.name;
+// Step 1: Create Players
+let player1 = {
+  name: prompt("Player 1 enter your name"), //Ask the players for their names (use propmt)
+  shipCount: 4,
+  // gameBoard: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 };
-
+let player2 = {
+  name: prompt("Player 2 enter your name"), //Ask the players for their names (use propmt)
+  shipCount: 4,
+  // gameBoard: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+};
+const board_Player1 = document.getElementById("board_player1");
+const board_Player2 = document.getElementById("board_player2");
+if (player1.name && player2.name != null) {
+  document.getElementById("name_player1").innerHTML = player1.name;
+  document.getElementById("name_player2").innerHTML = player2.name;
+}
 //Add lives to HTML
-document.getElementById('ships_player1').innerHTML = player1.shipCount;
-document.getElementById('ships_player2').innerHTML = player2.shipCount;
+document.getElementById("ships_player1").innerHTML = player1.shipCount;
+document.getElementById("ships_player2").innerHTML = player2.shipCount;
+
+
 
 //Function to get boards
-function playerBoard (boardId) {
-
+function playerBoard(boardId, player) {
   const board = document.getElementById(boardId);
   for (var x = 0; x < 4; x++) {
-
-  const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
-
-  for (var y = 0; y < 4; y++) {
-    const cell = document.createElement('div');
-    cell.className = "square"; // adding css properties to make it looks like a square
-    cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
-    cell.value = 0;//state of the cell
-
-    //this function adds the click event to each cell
-    cell.addEventListener( 'click', (e) => {
+    const li = document.createElement("li"); // creating childs for the list (board), in this case represent a row number 'x' of the board
+    for (var y = 0; y < 4; y++) {
+      const cell = document.createElement("div");
+      cell.className = "square"; // adding css properties to make it looks like a square
+      cell.textContent = `${x},${y}`; // saves the coordinates as a string value 'x,y'
+      cell.value = 0; //state of the cell
+      //this function adds the click event to each cell
+      cell.addEventListener("click", e => {
         let cell = e.target; // get the element clicked
-        console.log(cell.textContent) //display the coordinates in the console
-        cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
-        cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. 
+        console.log(cell.textContent); //display the coordinates in the console
+        cell.style.visibility = "hidden"; // this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
+        // cell.style.background = "purple"; //with this propertie you can change the background color of the clicked cell.
         // try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
-    });
-    li.appendChild(cell); //adding each cell into the row number x
+        shoot(cell);
+      });
+      li.appendChild(cell); //adding each cell into the row number x
+    }
+    board.appendChild(li); //adding each row into the board
+    player.gameBoard = board; 
   }
-  board.appendChild(li); //adding each row into the board
+}
+playerBoard("board_player1", player1);
+playerBoard("board_player2", player2);
+
+//Function to positioning ships 
+  // placeShips('board_player2');
+
+const placeShips = boardId => {
+  console.log(boardId);
+  // const startCoordinates = { x: 0, y: 0 };
+  let shipPlaced = 0;
+  //loop that runs until 4 ships have been added to the board
+  while (shipPlaced < 4) {
+    // console.log('ships placed start', shipPlaced)
+    // let { x, y } = startCoordinates;
+    // Inside the loop, generate a random x and a random y coordinate (must be between 0 and 3)
+    let x = Math.floor(Math.random() * Math.floor(4));
+    let y = Math.floor(Math.random() * Math.floor(4));
+    const board = document.getElementById(boardId);
+    let boardRow = board.getElementsByTagName("li")[x];
+    // console.log("row", boardRow);
+    // console.log(boardRow);
+    let boardCell = boardRow.getElementsByTagName("div")[y];
+    // console.log(boardCell.value);
+    if (boardCell.value !== 1) {
+      console.log(boardCell.value)
+      boardCell.value = 1;
+      console.log(boardCell.value, "placed a ship");
+      // extra debugging.
+      boardCell.innerHTML = "1"
+      shipPlaced++;
+    }
+    console.log(shipPlaced)
   }
 };
-const player1Board = playerBoard('board_player1');
-const player2Board = playerBoard('board_player2');
+placeShips("board_player1");
+placeShips("board_player2");
+const board = document.getElementById("board_player1");
+// console.log(board);
 
-/*
-//Function to positioning ships (Isn't working) 
 
-const placeShips = (board) => {
-  const startCoordinates = {x: 0, y:0};
-  let shipPlaced = 0; 
-  //loop that runs until 4 ships have been added to the board
-  while (shipPlaced < 4){
-    let {x, y} = startCoordinates;
-    // Inside the loop, generate a random x and a random y coordinate (must be between 0 and 3)
-    x = Math.floor(Math.random() * Math.floor(4));
-    y = Math.floor(Math.random() * Math.floor(4));
-    let boardRow = board.getElementsByTagName('li')[x];
-    let boardCell = boardRow.getElementsByTagName('div')[y];
-    if (boardCell.value !== 1) {
-      boardCell.value = 1;
-      console.log(boardCell.innerText);
-      shipPlaced ++;   
-      } 
-    }
-  };
-  placeShips('board_player1');
-  // placeShips('board_player2');
-*/
-  //I had this from the previous game:
-  let shipPlaced1 = 0; 
-  let shipPlaced2 = 0; 
-  // 1.-Create a loop that runs until 4 ships have been added to the board
-  while (shipPlaced1 < 4 && shipPlaced2 <4){
-  // Inside the loop, generate a random x and a random y coordinate (must be between 0 and 3)
-  let xPos1 = Math.floor(Math.random() * 4);
-  let xPos2 = Math.floor(Math.random() * 4);
-  // console.log(xPos);
-  let yPos1 = Math.floor(Math.random() * 4);
-  let yPos2 = Math.floor(Math.random() * 4);
-  // console.log(yPos);
-  let shipRandom1 = [xPos1, yPos1]
-  let shipRandom2 = [xPos2, yPos2]
-    console.log(shipRandom1);
-    console.log(shipRandom2);
-  if (player1.gameBoard[xPos1][yPos1] === 0 && player2.gameBoard[xPos2][yPos2] === 0) {
-    player1.gameBoard[xPos1][yPos1] = 1;
-    player2.gameBoard[xPos2][yPos2] = 1;
-    shipPlaced1 ++;
-    shipPlaced2 ++;
-     
-    } 
+
+
+//Set player turns
+function whoIsPlaying(currentPlayer){  
+
+  if (currentPlayer === player1){
+    document.getElementById("board_player2").style.display = "none";
+    document.getElementById("board_player1").style.display = "block";
   }
-  console.log(player1.gameBoard);  
-  console.log(player2.gameBoard);
+  if (currentPlayer === player2) {
+    document.getElementById("board_player1").style.display = "none";
+    document.getElementById("board_player2").style.display = "block";
+    }
+}
+whoIsPlaying(player1);
+
+
+//Shooting function 
+
+// function shoot(cell){
+  
+//   if (cell.textContent == 1){
+//     alert("Hit!!!")
+//     cell.textContent = 0
+//     player1.shipCount --;
+//     console.log(player1.shipCount) 
+//   }else {
+//     alert("Missed!!!")
+//     whoIsPlaying(player2);
+//   }
+//   }
+//   // return;
+
+let currentUser = player1.name;
+  // let message;
+  
+while (player1.ships !== 0 || player2.ships !== 0) {
+  function shoot(cell){
+  if (currentUser === player1.name) {
+    if (cell.textContent == 1) {
+      player1.shipCount --;
+        alert (`Hit!`)
+      } else if (player1.shipCount === 0) {
+        alert (`Congratulations ${currentUser} you WIN!!!`)
+        break;
+      } else {
+        alert (`You missed!`);
+        currentUser = player2.name; 
+      }
+  }
+    if (currentUser === player2.name) {
+      if (cell.textContent == 1) {
+        player2.shipCount --;
+          alert (`Hit!`)
+        } else if (player2.shipCount === 0) {
+          alert (`Congratulations ${currentUser} you WIN!!!`)
+          break;
+        } else {
+          alert (`You missed!`);
+          currentUser = player1.name; 
+        }
+      }
+    }
+  }
+
+
+// Track current player 
+// assing player later 
+// Switch function 
